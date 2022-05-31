846e8644ca594bfe13c061e8cfa72f6ab09dfe77
Kiki <kimdirectkim@yandex.ru>
2022-05-31 12:16:19 +0300

    Добавлена кнопка пропуска, поправлена адаптивность

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ $(document).ready(function() { // Ждём загрузки страницы
 	$(steps[0]).show(); // показываем первый шаг
 	var current_step = 0; // задаем текущий шаг
 	var question_results = document.getElementsByClassName("question-results");
-
-	$("a.next").click(function(){	// Событие клика на ссылку "Следующий шаг"
+	var navigation = document.getElementsByClassName("navigation");
+	var missQuestion = document.getElementsByClassName("missQuestion")
+	$(missQuestion).click(function(){	// Событие клика на ссылку "Пропустить шаг"
         if (current_step == steps.length-2) { // проверяем, будет ли следующий шаг последним
 				$(this).hide(); // скрываем ссылку "Следующий шаг"
+				$(navigation).hide();
 				$("form input[type=submit]").show(); // показываем кнопку "Регистрация"
 				$(question_results).hide();
 			}
@@ -16,6 +18,22 @@ $(document).ready(function() { // Ждём загрузки страницы
 			changeStep(current_step); // меняем шаг
             var progress = 100 / stepsLength * current_step;
             document.getElementById("myBar").style.width = progress + "%";
+			indicator.innerHTML= `<p class="indicator">${current_step+1}/${steps.length}</p>`;
+	});
+
+	$("a.next").click(function(){	// Событие клика на ссылку "Следующий шаг"
+        if (current_step == steps.length-2) { // проверяем, будет ли следующий шаг последним
+				$(this).hide(); // скрываем ссылку "Следующий шаг"
+				$(navigation).hide();
+				$("form input[type=submit]").show(); // показываем кнопку "Регистрация"
+				$(question_results).hide();
+			}
+			$("a.back").show(); // показываем ссылку "Назад"
+			current_step++; // увеличиваем счетчик текущего слайда
+			changeStep(current_step); // меняем шаг
+            var progress = 100 / stepsLength * current_step;
+            document.getElementById("myBar").style.width = progress + "%";
+			indicator.innerHTML= `<p class="indicator">${current_step+1}/${steps.length}</p>`;
 	});
 
 	$("a.back").click(function(){	// Событие клика на маленькое изображение
@@ -29,6 +47,7 @@ $(document).ready(function() { // Ждём загрузки страницы
 		changeStep(current_step);// меняем шаг
         var progress = 100 / stepsLength * current_step;
         document.getElementById("myBar").style.width = progress + "%";
+		indicator.innerHTML= `<p class="indicator">${current_step+1}/${steps.length}</p>`;
 	});
 	
 	function changeStep(i) { // функция смены шага
