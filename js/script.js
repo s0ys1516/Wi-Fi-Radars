955ae59b428075926791d5cbd45c64d2e087174e
Kiki <kimdirectkim@yandex.ru>
2022-06-21 16:37:42 +0300

    Обработка формы

--- js/script.js
+++ js/script.js
@@ -56,3 +56,96 @@ $(document).ready(function() { // Ждём загрузки страницы
 	}
 });
 
+// Слайдеры
+
+const swiper = new Swiper('.swiper-how-it-works', {
+	speed: 400,
+	spaceBetween: 100,
+	navigation: {
+		nextEl: '.slider-button-next',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	direction:'vertical',
+	mousewheel:{
+		sansitivity: 1,
+	},
+  });
+  
+  const swiper2 = new Swiper('.swiper-how-can-be', {
+	speed: 400,
+	spaceBetween: 100,
+	spaceBetween: 30,
+	breakpoints: {
+		360: {
+			slidesPerView: 1.5,
+		},
+		768: {
+			slidesPerView: 1.5,
+		},
+		1440: {
+			slidesPerView: 3,
+		}
+	},
+  });
+
+
+// Работа с формой
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', form_Send);
+
+  async function form_Send(e) {
+	e.preventDefault();
+
+	let error = formValidate(form);
+
+	let formData = new FormData(form);
+
+	if (error===0){
+		form.classList.add('_sending')
+		let response = await fetch ('sendmail.php', {
+			method:'POST',
+			body: formData
+		});
+		if (response.ok){
+			let result = await response.json();
+			alert(result.message);
+			form.reset();
+			form.classList.remove('_sending');
+		}else{
+			form.classList.remove('_sending');
+		}
+	}else{
+
+	}
+  }
+
+
+  function formValidate(form){
+		let error = 0;
+		let formReq = document.querySelectorAll('._req')
+
+		for (let index = o; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input.classList.contains);
+
+			if (input.value === '') {
+				formAddError(input);
+				error++;
+			} 
+		}
+		return error;
+	}
+
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+});
